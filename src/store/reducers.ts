4219175d043efa16893d4942f529336961485a71
Rene Vallecillo <reneval@gmail.com>
2017-12-09 19:06:35 -0600

    adding implementations while on UA course

---
+++ src/store/reducers.ts
@@ -0,0 +1,34 @@
+import * as fromActions from "./actions";
+
+export const initialState = {
+  loaded: false,
+  loading: false,
+  data: [{ label: "Eat Pizza", complete: false }]
+};
+
+export function reducer(
+  state = initialState,
+  action: { type: string; payload: any }
+) {
+  switch (action.type) {
+    case fromActions.ADD_TODO: {
+      const todo = action.payload;
+      const data = [...state.data, todo];
+      return {
+        ...state,
+        data: data //same as leaving just data
+      };
+    }
+    case fromActions.REMOVE_TODO: {
+      console.log(action.payload);
+      const data = state.data.filter(
+        todo => todo.label !== action.payload.label
+      );
+      return {
+        ...state,
+        data
+      };
+    }
+  }
+  return state;
+}
